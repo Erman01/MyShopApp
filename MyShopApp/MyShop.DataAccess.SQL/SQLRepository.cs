cb9ccb5b1ede5e779c79aacc21aeea762a12fd90
Erman Kemanci <ekemanci01@hotmail.com>
2020-04-17 23:54:42 +0100

    Implemented SQL Repository

---
+++ MyShopApp/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,55 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+        public SQLRepository(DataContext _context)
+        {
+            context = _context;
+            this.dbSet = context.Set<T>();
+        }
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var t = dbSet.Find(id);
+            if (context.Entry(t).State == EntityState.Detached)
+                dbSet.Attach(t);
+            dbSet.Remove(t);
+        }
+
+        public T Find(string id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(T Tentity)
+        {
+            dbSet.Add(Tentity);
+        }
+
+        public void Update(T Tentity)
+        {
+            dbSet.Attach(Tentity);
+            context.Entry(Tentity).State = EntityState.Modified;
+        }
+    }
+}
